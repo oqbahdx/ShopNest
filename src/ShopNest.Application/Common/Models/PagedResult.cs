7b72e5e17f4edab6fe9782c3c7d412d08611e078
oqbahdx <oqbahdx@gamil.com>
2026-05-29 12:33:37 +0300

    cart - wishlist - cupons features Done

--- src/ShopNest.Application/Common/Models/PagedResult.cs
+++ src/ShopNest.Application/Common/Models/PagedResult.cs
@@ -1,25 +1,37 @@
+using System;
+using System.Collections.Generic;
+
 namespace ShopNest.Application.Common.Models;
 
 public sealed class PagedResult<T>
 {
-    public IReadOnlyList<T> Items { get; init; } = [];
-    public int Page { get; init; }
-    public int PageSize { get; init; }
-    public int TotalCount { get; init; }
-    public int TotalPages => (int)Math.Ceiling(TotalCount / (double)PageSize);
-    public bool HasPreviousPage => Page > 1;
-    public bool HasNextPage => Page < TotalPages;
+	public IReadOnlyList<T> Items { get; init; } = Array.Empty<T>();
 
-    public static PagedResult<T> Create(
-        IReadOnlyList<T> items, int page, int pageSize, int totalCount)
-        => new()
-        {
-            Items = items,
-            Page = page,
-            PageSize = pageSize,
-            TotalCount = totalCount
-        };
+	public int Page { get; init; }
 
-    public static PagedResult<T> Empty(int page, int pageSize)
-        => Create([], page, pageSize, 0);
+	public int PageSize { get; init; }
+
+	public int TotalCount { get; init; }
+
+	public int TotalPages => (int)Math.Ceiling((double)TotalCount / (double)PageSize);
+
+	public bool HasPreviousPage => Page > 1;
+
+	public bool HasNextPage => Page < TotalPages;
+
+	public static PagedResult<T> Create(IReadOnlyList<T> items, int page, int pageSize, int totalCount)
+	{
+		return new PagedResult<T>
+		{
+			Items = items,
+			Page = page,
+			PageSize = pageSize,
+			TotalCount = totalCount
+		};
+	}
+
+	public static PagedResult<T> Empty(int page, int pageSize)
+	{
+		return Create(Array.Empty<T>(), page, pageSize, 0);
+	}
 }
--- src/ShopNest.Application/DependencyInjection.cs
+++ src/ShopNest.Application/DependencyInjection.cs
@@ -1,19 +1,23 @@
+using System;
+using System.Reflection;
+using AutoMapper;
 using FluentValidation;
-using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ShopNest.Application;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
-    {
-        services.AddMediatR(cfg =>
-            cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
-
-        services.AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly);
-        services.AddAutoMapper(_ => { }, typeof(DependencyInjection).Assembly);
-
-        return services;
-    }
+	public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+	{
+		services.AddMediatR(delegate(MediatRServiceConfiguration cfg)
+		{
+			cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+		});
+		services.AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly);
+		services.AddAutoMapper((Action<IMapperConfigurationExpression>)delegate
+		{
+		}, new Assembly[1] { typeof(DependencyInjection).Assembly });
+		return services;
+	}
 }
